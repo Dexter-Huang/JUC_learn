917e04a2c396debeb76382b6a8785601a10608b5
Dexter-Huang <12011828@mail.sustech.edu.cn>
2023-09-06 22:26:01 +0800

    feat((Test)RedisConfig.java): (Test)RedisConfig.java

---
+++ Test/src/main/java/com/example/tikatest/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.example.tikatest.config;
+
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory){
+        RedisMessageListenerContainer messageListenerContainer = new RedisMessageListenerContainer();
+        messageListenerContainer.setConnectionFactory(connectionFactory);
+        return messageListenerContainer;
+    }
+}
